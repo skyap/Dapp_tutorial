0a76d56020397b399a16ede8e17198ffa2145fa3
skyap <albert_einstein_1879_3_14@hotmail.com>
2023-08-15 12:09:03 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Wallet from './components/Wallet';
+
+function App() {
+  return (
+    <div className="App">
+      <Wallet />
+    </div>
+  );
+}
+
+export default App;
